4f5f362ebfefbad699d1c65a609561bd86c31b6e
ENVY14\Alphagone <990131@naver.com>
2022-01-20 16:11:15 +0900

    파워쉘 객체 using문을 사용하여 선언

--- BasicAppRemover/BasicAppRemover.cs
+++ BasicAppRemover/BasicAppRemover.cs
@@ -1,95 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Management.Automation;
 using System.Collections;
+using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Windows.Forms;
 
 namespace BasicAppRemover
 {
-    
+
     public partial class BasicAppRemover : Form
     {
-        
-        
-
         public BasicAppRemover()
         {
             InitializeComponent();
 
             initData();
             btnRemove.Click += BtnRemove_Click;
-            
+            runSpace = RunspaceFactory.CreateRunspace();
+            runSpace.Open();
         }
+        Runspace runSpace;
 
         private void BtnRemove_Click(object sender, EventArgs e)
         {
-            
+
             ArrayList arrayList = new ArrayList();
 
-            for(int i = 0; i < checkedListPackage.CheckedItems.Count; i++)   
+            for (int i = 0; i < checkedListPackage.CheckedItems.Count; i++)
             {
-                arrayList.Add(checkedListPackage.CheckedItems[i].ToString());    
+                arrayList.Add(checkedListPackage.CheckedItems[i].ToString());
             }
-             
+
             //최종 확인 팝업 띄우기
             checkPackage CheckPop = new checkPackage(arrayList.ToArray());
 
             //삭제를 누르면 삭제 진행
-            if (CheckPop.ShowDialog() == DialogResult.OK) {
+            if (CheckPop.ShowDialog() == DialogResult.OK)
+            {
                 MessageBox.Show("삭제를 진행합니다.");
 
-                Runspace runSpace = RunspaceFactory.CreateRunspace();
-                runSpace.Open();
-                PowerShell ps = PowerShell.Create();
-                ps.Runspace = runSpace;
-  
-                foreach (object value in arrayList.ToArray()) {
-                    ps.AddCommand("Get-AppxPackage");
-                    ps.AddArgument("AllUsers");;  
-                    ps.AddParameter("name", "*" + value.ToString().Trim() + "*");
-                    ps.AddCommand("Remove-AppxPackage");
-                    ps.AddStatement();
-                    //try
-                    //{
-                    //    ps.AddCommand("Remove-AppxPackage");
-                    //    ps.AddParameter("AllUsers");
-                    //    ps.AddParameter("Package", "*" + value.ToString() + "*");
-                    //    ps.AddStatement();
-                    //}
-                    //catch (Exception)
-                    //{
 
-                    //    throw;
-                    //}
-                    
-                    
-                }
-
-                try
+                using (PowerShell ps = PowerShell.Create())
                 {
-                    ps.Invoke();
+                    ps.Runspace = runSpace;
 
-                }
-                catch (Exception) {
-                    throw;
-                }
+                    foreach (object value in arrayList.ToArray())
+                    {
+                        ps.AddCommand("Get-AppxPackage");
+                        ps.AddParameter("AllUsers");
+                        ps.AddParameter("name", "*" + value.ToString().Trim() + "*");
+                        ps.AddCommand("Remove-AppxPackage");
+                        Console.WriteLine(ps.Commands.Commands[0].ToString() + " " +
+                                            ps.Commands.Commands[0].Parameters[0].Name.ToString() +" " +
+                                            ps.Commands.Commands[0].Parameters[0].Value.ToString() + " " +
+                                            ps.Commands.Commands[0].Parameters[1].Name.ToString() + " " +
+                                            ps.Commands.Commands[0].Parameters[1].Value.ToString() + "|" + 
+                                            ps.Commands.Commands[1].ToString());
+                        ps.Invoke();
+                    }
 
-                //ps.Invoke();
-                //checkedListPackage.Items.Clear();
-                //foreach (PSObject result in ps.Invoke())
-                //{
-                //    checkedListPackage.Items.Add(result.ToString());
-                //}
-                ps.Dispose();
-                runSpace.Dispose();
-                initData();
+                    Console.WriteLine("명령어 실행 완료");
+                    initData();
+                }
             }
             else
             {
@@ -97,27 +68,25 @@ namespace BasicAppRemover
             }
         }
 
-        private void initData()
+        private void initData()     
         {
-            Runspace runSpace = RunspaceFactory.CreateRunspace();
-            runSpace.Open();
-            PowerShell ps = PowerShell.Create();
-            ps.Runspace = runSpace;
-            
             checkedListPackage.Items.Clear();
 
-            //설치된 패키지들의 이름만 검색
-            ps.AddCommand("Get-AppxPackage");           
-            ps.AddCommand("Select-Object");
-            ps.AddArgument("Name");
-
-            foreach (PSObject result in ps.Invoke())
+            using (PowerShell ps = PowerShell.Create())
             {
-                checkedListPackage.Items.Add(result.ToString().Replace("@{Name=","").Replace('}',' '));  
-                //checkedListPackage.Items.Add(result.ToString().Replace("@{PackageFullName=", "").Replace('}', ' '));
+                ps.Runspace = runSpace;
+
+                //설치된 패키지들의 이름만 검색
+                ps.AddCommand("Get-AppxPackage");
+                ps.AddCommand("Select-Object");
+                ps.AddArgument("Name");
+
+                foreach (PSObject result in ps.Invoke())
+                {
+                    checkedListPackage.Items.Add(result.ToString().Replace("@{Name=", "").Replace('}', ' '));
+                    //checkedListPackage.Items.Add(result.ToString().Replace("@{PackageFullName=", "").Replace('}', ' '));
+                }
             }
-            ps.Dispose();
-            runSpace.Dispose();
         }
     }
 }
